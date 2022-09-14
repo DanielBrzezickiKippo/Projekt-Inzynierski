b5b1a6f98b275fba1482261d7f7db7bcb7e781ed
danciak <danielbrzezickikippo@gmail.com>
2022-09-14 21:01:31 +0200

    Project idea

---
+++ ProjektInzynierski/Assets/Sceipts/Interpreter.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interpreter : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    bool possibleToCompile(string code)
+    {
+        return false;
+    }
+
+    void interprete(string code)
+    {
+        if (possibleToCompile(code))
+        {
+
+        }
+    }
+
+
+    void makeAction(string command)
+    {
+        switch (command)
+        {
+            case "move":
+                break;
+
+        }
+    }
+
+}
